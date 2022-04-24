de118346c9d11f4c2218af447403d0014651a6ff
mouhcineToumi <mouhcine.toumi98@gmail.com>
2022-04-24 16:22:58 +0000

    add - get page_count and next_page from MoteurList

--- moteurmapy/moteur_list.py
+++ moteurmapy/moteur_list.py
@@ -2,6 +2,8 @@
 from moteurmapy.helpers import sanitize
 from moteurmapy.static_crawler import StaticCrawler
 import fuckit
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
 
 
 class MoteurList( StaticCrawler ):
@@ -19,10 +21,27 @@ class MoteurList( StaticCrawler ):
         d['model_year'] = html.find(class_="icon-normal-calendar-month").parent.text
         d['city'] = html.find(class_="icon-normal-pointer").parent.text
         d['fuel'] = html.find(class_="icon-fuel").parent.text
+    
+    @fuckit
+    def get_page_count(self, html):
+        d = {}
+        # get total page count
+        page_count = html.find('div', {'id': 'label-recherche-auto'}).find('span').text.split('sur')[-1].strip().split(' ')[0]
+        d['page_count'] = int(page_count)
+
+        # get next page number
+        pages = html.find(class_="pagination").find_all('a')
+        next_page = list(filter( lambda x: x.text.strip() == 'Suivante', pages))[0]
+        parsed = urlparse(next_page.get('href'))
+        captured_value = parse_qs(parsed.query)['per_page'][0]
+        d['next_page'] = 1+int(captured_value)//15
         return d
+    
     
     def run(self, url):
         html = self.get_html(url)
+        page_count = self.get_page_count(html)
         items = html.find_all(class_="row-item")
         data = list(map( self.parse, items))
-        return data
+        return data, page_count
+    
